7c65a3c051b6614c7ae995284e3f448c99cff61f
Clarice0997 <clarice09975@gmail.com>
2022-12-09 01:08:03 +0800

    feat: 用户详细页基本页面布局&页面逻辑完成

--- pages/userDetail/userDetail.js
+++ pages/userDetail/userDetail.js
@@ -1,12 +1,54 @@
 // pages/userDetail/userDetail.js
 const app = getApp()
-import lottie from 'lottie-miniprogram'
+import { initLottieCanvas } from '../../utils/lottie'
 
 Page({
   /**
    * 页面的初始数据
    */
-  data: {},
+  data: {
+    // 用户信息对象
+    userInfo: {
+      nickname: '',
+      phone: '',
+      sex: '男',
+      email: ''
+    },
+    showPop: false,
+    sexColumns: ['男', '女'],
+    sexIndex: 0
+  },
+  // 输入框改变事件
+  formNicknameChange(e) {
+    this.setData({
+      ['userInfo.' + e.currentTarget.id]: e.detail.value
+    })
+  },
+  // 点击展示弹出层事件
+  showPopup() {
+    this.setData({
+      showPop: true
+    })
+  },
+  // 关闭弹出层事件
+  closePopup() {
+    this.setData({
+      showPop: false
+    })
+  },
+  // 选择性别确定事件
+  onConfirmSex(event) {
+    const { value, index } = event.detail
+    // 赋值
+    this.setData({
+      sexIndex: index,
+      'userInfo.sex': this.data.sexColumns[index]
+    })
+  },
+  // 确认修改按钮点击事件
+  clickHandler() {
+    console.log(this.data.userInfo)
+  },
 
   /**
    * 生命周期函数--监听页面加载
@@ -21,7 +63,9 @@ Page({
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow() {},
+  onShow() {
+    initLottieCanvas('#canvas', 'https://assets5.lottiefiles.com/private_files/lf30_4bVja9.json')
+  },
 
   /**
    * 生命周期函数--监听页面隐藏
